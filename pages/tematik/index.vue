52f50c23e4dcca0fa9e1b0aabc736044a9e44df2
mbahnjend <139411264+MbahNjend@users.noreply.github.com>
2024-10-09 15:06:40 +0700

    Update Commit

--- pages/tematik/index.vue
+++ pages/tematik/index.vue
@@ -72,7 +72,9 @@ const applySelection = () => {
             <option value="25">25</option>
             <option value="50">50</option>
             <option value="75">75</option>
-            <option value="100" selected>100</option>
+            <option value="100">100</option>
+            <option value="150">150</option>
+            <option value="200" selected>200</option>
           </select>
           <span>Entries</span>
         </div>
